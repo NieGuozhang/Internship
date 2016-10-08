221241814e2881d8b2647a175109681e21bdaa38
Nie <Nie@DESKTOP-AQJ0DV8>
2016-10-09 00:02:48 +0800

    10月09日，缩减项目大小

--- 实习帮/src/com/hbut/internship/activity/FindInternshipSearchActivity.java
+++ 实习帮/src/com/hbut/internship/activity/FindInternshipSearchActivity.java
@@ -11,8 +11,6 @@ import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
